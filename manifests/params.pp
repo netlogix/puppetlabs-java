d8b2db257beb1ff69021b49623e26d4c76a4e75b
Andre Ponert <andre.ponert@netlogix.de>
2019-02-21 14:40:58 +0100

    [TASK] Added ubuntu bionic support

--- manifests/params.pp
+++ manifests/params.pp
@@ -164,7 +164,7 @@ class java::params {
             },
           }
         }
-        'vivid', 'wily', 'xenial': {
+        'vivid', 'wily', 'xenial', 'bionic': {
           $java =  {
             'jdk' => {
               'package'          => 'openjdk-8-jdk',
